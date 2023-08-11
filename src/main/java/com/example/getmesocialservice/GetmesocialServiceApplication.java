6279bc970237e3caa90e07b734eb2a3d83feef3a
pavitra-997 <100881473+pavitra-997@users.noreply.github.com>
2023-08-11 10:44:30 -0400

    swagger commit 1

--- src/main/java/com/example/getmesocialservice/GetmesocialServiceApplication.java
+++ src/main/java/com/example/getmesocialservice/GetmesocialServiceApplication.java
@@ -2,17 +2,16 @@ package com.example.getmesocialservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableSwagger2
 @SpringBootApplication
 public class GetmesocialServiceApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(GetmesocialServiceApplication.class, args);
 	}
-
 
 }
 
