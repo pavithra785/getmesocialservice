148b1fc78568af0b852e4040e313e73e21943f80
pavitra-997 <100881473+pavitra-997@users.noreply.github.com>
2024-04-26 10:54:51 -0400

    my frst commit

--- src/main/java/com/example/getmesocialservice/resource/CommentResource.java
+++ src/main/java/com/example/getmesocialservice/resource/CommentResource.java
@@ -55,7 +55,7 @@ public class CommentResource {
         return null;
     }
 
-    @DeleteMapping
+    @DeleteMappingcd
     public void deleteComment( @RequestParam (name = "commentId") String commentId,@RequestHeader(name="idToken")String idToken ) throws IOException, FirebaseAuthException {
         FirebaseUser firebaseUser = firebaseService.authenticate(idToken);
         if(firebaseUser!=null){
