d42b54af0dc30feefba2f9ffe09cb738e5011eae
pavithra785 <55778487+pavithra785@users.noreply.github.com>
2024-04-28 11:57:03 -0400

    Update AlbumResource.java

--- src/main/java/com/example/getmesocialservice/resource/AlbumResource.java
+++ src/main/java/com/example/getmesocialservice/resource/AlbumResource.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/album")
 public class AlbumResource {
