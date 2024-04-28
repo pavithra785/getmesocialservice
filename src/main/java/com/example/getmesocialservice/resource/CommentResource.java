f76acaf3fbe4c34379e169a8d579242b41a876dd
pavithra785 <55778487+pavithra785@users.noreply.github.com>
2024-04-28 11:57:43 -0400

    Update CommentResource.java

--- src/main/java/com/example/getmesocialservice/resource/CommentResource.java
+++ src/main/java/com/example/getmesocialservice/resource/CommentResource.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/comment")
 public class CommentResource {
